3f6ca97be79f38635c5c0214a1ec0d4f53bda99b
Very lixo <ryofreefire32@gmail.com>
2025-12-16 13:48:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,41 +11,54 @@
 # theres no documentation for ts, if u (idk why u would do that) use this leanguege, read the interpreter #
 ###########################################################################################################
 
-import sys
+import sys #only 1 library? +999 aura
 
 path = sys.argv[1] #chatGPT also said is imporant to validate and bla bla bla, but i dont think like him
-variables = {}
+variables = {} #stairs
+Params = {} #stairs
+fns = {} #stairs
 skip = False
+current_func = None
 
-try: #chatGPT told me thats important \_('-')_/
-    with open(path, "r") as file: #its also a chatGPT ideia, prob the most useless shit ever created
+try:
+    with open(path, "r") as file:
         for line in file:
             tokens = line.strip().split()
 
             if not tokens:
                 continue
 
+            if current_func:
+                if tokens[0] == "ItsOver":
+                    current_func = None
+                    skip = False
+                else:
+                    fns[current_func]["body"].append(line.strip())
+                continue
+
             if skip:
-                if tokens[0] == "8=D": #cock of destiny (this is for makes the if ends, sounds nice until u draw 13 cocks per code)
+                if tokens[0] == "8=D": #cock of the destiny
                     skip = False
                 continue
 
-            if tokens[0] == "var": #idk wtf tokens mean, but i think thats a cool name
+            if tokens[0] == "var":
                 typ = tokens[1]
                 name = tokens[2]
                 value = tokens[3]
 
-                if typ == "int": #interger (i wrote this right?)
+                if typ == "int":
                     variables[name] = int(value)
-                elif typ == "str": #string ig
+                elif typ == "str":
                     variables[name] = value
                 else:
                     print("type error")
+                    print("code = 1")
                     sys.exit(1)
 
-            elif tokens[0] == "print": #thats not for printing the screen
+            elif tokens[0] == "print":
                 if len(tokens) != 2:
                     print("syntax error")
+                    print("code = 1")
                     sys.exit(1)
 
                 name = tokens[1]
@@ -54,9 +67,10 @@ try: #chatGPT told me thats important \_('-')_/
                 else:
                     print(name)
 
-            elif tokens[0] == "if": #comparation type shi (i wrote comparation right?)
+            elif tokens[0] == "if":
                 if len(tokens) != 4:
                     print("syntax error")
+                    print("code = 1")
                     sys.exit(1)
 
                 left, op, right = tokens[1], tokens[2], tokens[3]
@@ -64,12 +78,7 @@ try: #chatGPT told me thats important \_('-')_/
                 left_val = variables[left] if left in variables else int(left)
                 right_val = variables[right] if right in variables else int(right)
 
-                ##########################
-                # WORST PART OF THE CODE #
-                ##########################
-                #i sooooo good making desings man
-                                
-                if op == "==": 
+                if op == "==":
                     result = left_val == right_val
                 elif op == "!=":
                     result = left_val != right_val
@@ -83,23 +92,52 @@ try: #chatGPT told me thats important \_('-')_/
                     result = left_val >= right_val
                 else:
                     print("unknown operator")
+                    print("code = 1")
                     sys.exit(1)
 
                 if not result:
                     skip = True
-                
-                #it ended ig
 
-except FileNotFoundError: #chatGPT told me thats important
+            elif tokens[0] == "func": #i feel like a genious rn
+                fname = tokens[1]
+                params = tokens[2:] #idk why we need the ":", but chatGPT told me thats important
+
+                fns[fname] = {
+                    "params": params,
+                    "body": []
+                } #its json? idk
+
+                current_func = fname
+                skip = True
+
+            elif tokens[0] == "call": #this one was made by chatGPT ngl
+                fname = tokens[1]
+                args = tokens[2:]
+
+                if fname not in fns:
+                    print("function not found")
+                    print("code = 1")
+                    sys.exit(1)
+
+                func = fns[fname]
+
+                if len(args) != len(func["params"]):
+                    print("wrong number of args")
+                    print("code = 1")
+                    sys.exit(1)
+
+                local_vars = variables.copy()
+
+                for p, a in zip(func["params"], args):
+                    local_vars[p] = int(a) if a.isdigit() else variables.get(a, a)
+
+                for cmd in func["body"]:
+                    parts = cmd.split()
+                    if parts[0] == "print":
+                        name = parts[1]
+                        print(local_vars[name] if name in local_vars else name)
+
+except FileNotFoundError:
     print("file not found")
+    print("code = 1")
     sys.exit(1)
-
-#i feel like a hacker using this dark VScode theme
-#i also create web-applications
-#pay me to get the best website ever created (or not)
-#reasons why to do that:
-#Why not?
-#I program in flask
-#i know how to program (ig)
-#i beatiful (ig)
-#i prefer kasane teto instead of hatsune miku
